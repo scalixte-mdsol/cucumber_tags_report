c8fb581c8e59c06daff301ca13ef0533bf8dbb8d
Stanley Calixte <scalixte@mdsol.com>
2015-10-12 17:54:06 -0400

    reformat with change

--- lib/cucumber_tags_report.rb
+++ lib/cucumber_tags_report.rb
@@ -2,7 +2,7 @@ module CucumberTagsReport
   require_relative 'cucumber_tags_report/version'
   require_relative 'cucumber_tags_report/formatter/tags_list'
   require_relative 'cucumber_tags_report/formatter/tags_report'
-  require_relative 'cli/options'
-  require_relative 'cli/configuration'
-  require_relative 'cli/main'
+  require_relative 'cucumber_tags_report/cli/options'
+  require_relative 'cucumber_tags_report/cli/configuration'
+  require_relative 'cucumber_tags_report/cli/main'
 end
