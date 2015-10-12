b6455da45bcb3269b601799f3aaa87f73e69037f
Stanley Calixte <scalixte@mdsol.com>
2015-10-11 23:49:42 -0400

    update library to write report

--- lib/cucumber_tags_report/formatter/tags_report.rb
+++ lib/cucumber_tags_report/formatter/tags_report.rb
@@ -6,6 +6,99 @@ require 'cucumber/formatter/console'
 module CucumberTagsReport
   module Formatter
     class TagsReport
+      attr_reader :scenario_hashes, :scenario_tags, :scenario_hash, :feature_hash, :io, :options, :runtime
+      include Cucumber::Formatter::Io
+      include Cucumber::Formatter::Console
+
+      def initialize(runtime, path_or_io, options)
+        @runtime = runtime
+        @options = options
+        @io = ensure_io(path_or_io, "tags_report")
+
+        @scenario_tags = []
+        @scenario_hashes = []
+        @scenario_hash = {}
+        @feature_hash = {}
+      end
+
+      def before_features(features)
+      end
+
+      def before_feature(feature)
+        create_feature(feature)
+      end
+
+      def before_feature_element(feature_element)
+        case feature_element
+          when Cucumber::Ast::Scenario, Cucumber::Ast::ScenarioOutline
+            create_scenario(feature_element)
+        end
+      end
+
+      def after_feature_element(feature_element)
+        case feature_element
+          when Cucumber::Ast::Scenario, Cucumber::Ast::ScenarioOutline
+            @scenario_hashes << @scenario_hash
+            @scenario_hash = {}
+        end
+      end
+
+      def scenario_name(keyword, name, file_colon_line, source_indent)
+        @scenario_hash.merge!({complete_description: name})
+      end
+
+      def after_feature(feature)
+        @feature_hash = {}
+      end
+
+      def after_features(features)
+        done
+      end
+
+      private
+
+      def done
+        @io.write(MultiJson.dump(@scenario_hashes, pretty: true))
+      end
+
+      def current_feature
+        @feature_hash ||= {}
+      end
+
+      def same_feature_as_previous_test_case?(feature)
+        current_feature[:uri] == feature.file && current_feature[:line] == feature.location.line
+      end
+
+      def create_feature(feature)
+        unless same_feature_as_previous_test_case?(feature)
+          current = feature.gherkin_statement.to_hash
+          @feature_hash = {
+              uri: feature.file,
+              id: current['id'],
+              keyword: current['keyword'],
+              title: current['name'],
+              description: current['description'],
+              line: current['line'],
+              tags: (current['tags'].collect { |tag| tag['name'] } if current['tags']) || []
+          }
+
+        end
+      end
+
+      def create_scenario(scenario)
+        hash = scenario.gherkin_statement.to_hash
+        @scenario_hash ||= {}
+        @scenario_hash.merge!({
+                                  uri: @feature_hash[:uri],
+                                  id: hash['id'],
+                                  keyword: hash['keyword'],
+                                  title: hash['name'],
+                                  description: hash['description'],
+                                  line: hash['line'],
+                                  tags: (hash['tags'].collect { |tag| tag['name'] } if hash['tags']) || []
+                              })
+        @scenario_hash[:tags] += @feature_hash[:tags]
+      end
     end
   end
 end
