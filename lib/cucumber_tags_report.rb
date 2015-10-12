cb5329841a10f612b2c136a5b0a19086c022fa82
Stanley Calixte <scalixte@mdsol.com>
2015-10-12 17:37:59 -0400

    update

--- lib/cucumber_tags_report.rb
+++ lib/cucumber_tags_report.rb
@@ -1,5 +1,8 @@
 module CucumberTagsReport
-    require_relative 'cucumber_tags_report/version'
-    require_relative 'cucumber_tags_report/formatter/tags_list'
-    require_relative 'cucumber_tags_report/formatter/tags_report'
+  require_relative 'cucumber_tags_report/version'
+  require_relative 'cucumber_tags_report/formatter/tags_list'
+  require_relative 'cucumber_tags_report/formatter/tags_report'
+  require_relative 'cli/options'
+  require_relative 'cli/configuration'
+  require_relative 'cli/main'
 end
