c68e9cdfc1a535bd46a0f3984026204d6c21736e
Stanley Calixte <scalixte@mdsol.com>
2015-10-13 15:38:08 -0400

    update libraries

--- lib/cucumber_tags_report/cli/configuration.rb
+++ lib/cucumber_tags_report/cli/configuration.rb
@@ -1,9 +1,10 @@
 require_relative 'options'
+# require 'cucumber'
 require 'cucumber/cli/configuration'
 
 module CucumberTagsReport
   module Cli
-    class Options < Cucumber::Cli::Configuration
+    class Configuration < Cucumber::Cli::Configuration
 
     end
   end
--- lib/cucumber_tags_report/cli/main.rb
+++ lib/cucumber_tags_report/cli/main.rb
@@ -1,10 +1,22 @@
+begin
+  require 'gherkin'
+rescue LoadError
+  require 'rubygems'
+  require 'gherkin'
+end
+
 require_relative 'configuration'
+require 'cucumber'
 require 'cucumber/cli/main'
 
 module CucumberTagsReport
   module Cli
-    class Options < Cucumber::Cli::Main
-
+    class Main < Cucumber::Cli::Main
+      class << self
+        def execute(args)
+          new(args).execute!
+        end
+      end
     end
   end
 end
