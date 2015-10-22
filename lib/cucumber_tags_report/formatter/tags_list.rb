728954b469b84f9208fb6489cbf7c356eff1e5a6
Stanley Calixte <scalixte@mdsol.com>
2015-10-22 12:36:29 -0400

    attempt to verify output stream-part11

--- lib/cucumber_tags_report/formatter/tags_list.rb
+++ lib/cucumber_tags_report/formatter/tags_list.rb
@@ -16,6 +16,7 @@ module CucumberTagsReport
         @io = ensure_io(path_or_io, "tags_list")
         @all_tags = []
         @io.puts options
+        puts options
       end
 
       def tag_name(tag_name)
