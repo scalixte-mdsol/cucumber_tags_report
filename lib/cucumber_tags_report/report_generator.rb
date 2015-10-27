5516b07cdfa34d22692533e65c4617f6cbf1699a
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 13:03:14 -0400

    sad 5s8

--- lib/cucumber_tags_report/report_generator.rb
+++ lib/cucumber_tags_report/report_generator.rb
@@ -6,7 +6,7 @@ require 'cucumber/formatter/io'
 
 module CucumberTagsReport
   class ReportGenerator
-    attr_reader :file
+    attr_reader :file, :generator
     include Cucumber::Formatter::Io
 
     def initialize(file_io)
@@ -20,24 +20,44 @@ module CucumberTagsReport
       puts file_io[0]
       puts file_io.split[1]
       puts "------------"
-      ensure
+    ensure
+      load_generator
       # @file = ensure_io('file.csv', "csv_generator")
     end
 
     def generate_report(report_hash)
+      dir = File.expand_path(File.dirname(__FILE__))
+      puts dir
+      puts @generator
+
+      load_basic_report(report_hash)
+
+    end
+
+    private
+
+    def load_generator
+      begin
+        require 'yaml'
+        if (File.exist?(file='features/support/tags_list_formatter.yml'))
+          @generator=YAML.load(File.open(file))
+        else
+          @generator=YAML.load(File.open('generators/templates/tags_list_formatter.yml'))
+        end
+      rescue
+        @generator=nil
+        puts 'could not load content'
+      end
+    end
+
+    def load_basic_report(report_hash)
       CSV.open("file.csv", "w") do |csv|
         csv << report_hash.first.keys
         report_hash.each do |hash|
           csv << hash.values
         end
       end
-      # end
-      # FasterCSV.generate(@file) do |csv|
-      #   csv << report_hash.first.keys
-      #   report_hash.each do |hash|
-      #     csv << hash.values
-      #   end
-      # end
     end
+
   end
 end
