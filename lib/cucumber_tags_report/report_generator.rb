582056d58a8b7bc8bf706f737d877dcb2ea0ce11
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 11:38:27 -0400

    sad 5s6

--- lib/cucumber_tags_report/report_generator.rb
+++ lib/cucumber_tags_report/report_generator.rb
@@ -1,6 +1,7 @@
 require 'multi_json'
 require 'base64'
 require 'csv'
+# require 'fastercsv'
 require 'cucumber/formatter/io'
 
 module CucumberTagsReport
@@ -20,24 +21,26 @@ module CucumberTagsReport
       puts file_io.split
       puts "------------"
       ensure
-      @file = ensure_io('file.csv', "csv_generator")
+      # @file = ensure_io('file.csv', "csv_generator")
     end
 
     def generate_report(report_hash)
-      # CSV.open("data.csv", "w", headers: hashes.first.keys) do |csv|
-      #   hashes.each do |h|
-      #     # csv << h.values
-      #     CSV::Writer.generate(outfile) do |csv|
-      #       csv << ['c1', nil, '', '"', "\r\n", 'c2']
-      #     end
-      #   end
-      # end
-      CSV::Writer.generate(@file) do |csv|
-        csv << report_hash.first.keys
-        report_hash.each do |hash|
-          csv << hash.values
+      CSV.open("file.csv", "w", headers: hashes.first.keys) do |csv|
+        hashes.each do |h|
+          # csv << h.values
+          csv << report_hash.first.keys
+          report_hash.each do |hash|
+            csv << hash.values
+          end
         end
       end
+      # end
+      # FasterCSV.generate(@file) do |csv|
+      #   csv << report_hash.first.keys
+      #   report_hash.each do |hash|
+      #     csv << hash.values
+      #   end
+      # end
     end
   end
 end
