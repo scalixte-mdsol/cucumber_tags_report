1b317e7d9fdef439895390c64c119418dbe54950
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 10:46:57 -0400

    sad 4

--- lib/cucumber_tags_report/report_generator.rb
+++ lib/cucumber_tags_report/report_generator.rb
@@ -8,9 +8,9 @@ module CucumberTagsReport
 
     def initialize(file_io)
       puts "------------"
-      puts file_io['csv']
+      puts file_io.class
       puts "------------"
-      @file = ensure_io(file_io['csv'], "csv_generator")
+      @file = ensure_io('file.csv', "csv_generator")
     end
 
     def generate_report(report_hash)
