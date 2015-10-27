006aff0a1fcc78385a1372c4013a83ff90894e80
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 17:04:37 -0400

    incorrect version part 1

--- lib/cucumber_tags_report/generators/install/install_generator.rb
+++ lib/cucumber_tags_report/generators/install/install_generator.rb
@@ -1,10 +1,12 @@
 begin
-  if defined? Rails
-    require 'rails/generators'
-    require 'rails/generators/base'
-  end
-rescue
-  loadError
+#   if defined? Rails
+#     require 'rails/generators'
+#     require 'rails/generators/base'
+#   end
+# rescue
+#   loadError
+  require 'rails/generators'
+  require 'rails/generators/base'
 end
 
 module CucumberTagsReport
@@ -13,7 +15,7 @@ module CucumberTagsReport
       source_root File.expand_path('../templates', __FILE__)
       desc 'Add new configuration to cucumber'
 
-      class_option :autogenerate, :type => :boolean, :default => false, :desc => "Auto generate data content for formatter."
+      class_option :autogenerate, :type => :boolean, :default => false, :desc => "Generates the fomatter script to create a new report"
       def create_install
         empty_directory 'features/support'
         template '../templates/tags_list_formatter.yml', File.join('features/support/', "tags_list_formatter.yml")
