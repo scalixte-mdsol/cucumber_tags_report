99955484f0c0038cb718bf339de80c5c5f5feccf
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 16:59:12 -0400

    update require method call

--- lib/cucumber_tags_report.rb
+++ lib/cucumber_tags_report.rb
@@ -3,6 +3,7 @@ module CucumberTagsReport
   require_relative 'cucumber_tags_report/report_generator'
   require_relative 'cucumber_tags_report/formatter/tags_list'
   require_relative 'cucumber_tags_report/formatter/tags_report'
+  require_relative 'cucumber_tags_report/generators/install/install_generator' if defined? Rails
   # require_relative 'cucumber_tags_report/cli/options'
   # require_relative 'cucumber_tags_report/cli/configuration'
   # require_relative 'cucumber_tags_report/cli/main'
