c3a4589c7c63ba2c8779b7fc0ed595867e6c8b04
Stanley Calixte <scalixte@mdsol.com>
2015-10-27 17:28:04 -0400

    update part 1

--- lib/cucumber_tags_report/generators/cucumber_tags_report/install/install_generator.rb
+++ lib/cucumber_tags_report/generators/cucumber_tags_report/install/install_generator.rb
@@ -15,13 +15,13 @@ module CucumberTagsReport
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-      desc 'Add new configuration to cucumber'
+      # desc 'Add new configuration to cucumber'
 
-      class_option :autogenerate, :type => :boolean, :default => false, :desc => "Generates the fomatter script to create a new report"
+      # class_option :autogenerate, :type => :boolean, :default => false, :desc => "Generates the fomatter script to create a new report"
 
+      desc "Generates the fomatter script to create a new report"
       def create_tags_list_formatter
-        # empty_directory 'features/support'
-        template '../templates/tags_list_formatter.yml', File.join('features/support/', "tags_list_formatter.yml")
+        template 'tags_list_formatter.yml', File.join('features, support', "tags_list_formatter.yml")
       end
 
     end
