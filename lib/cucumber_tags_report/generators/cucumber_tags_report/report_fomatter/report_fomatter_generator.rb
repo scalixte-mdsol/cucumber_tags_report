22611ea18adf31251ada222e771a9ff4c72a2f86
Stanley Calixte <scalixte@mdsol.com>
2015-10-28 09:14:18 -0400

    add new file

---
+++ lib/cucumber_tags_report/generators/cucumber_tags_report/report_fomatter/report_fomatter_generator.rb
@@ -0,0 +1,10 @@
+require 'rails/generators'
+require 'rails/generators/base'
+
+module Gemini
+  module Generators
+    class ReportFomatterGenerator < Rails::Generators::Base
+
+    end
+  end
+end
